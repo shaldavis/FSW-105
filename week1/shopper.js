f3b471bf97222447645638eacb55f721e01838a2
shaldavis <shal.davis@bryanuniversity.edu>
2020-08-27 19:29:53 -0400

    commit

---
+++ week1/shopper.js
@@ -0,0 +1,9 @@
+var shopper = {
+    name: "Donnie",
+    wallet: 80,
+    hasMoney: true,
+    buy: function() {
+        return this.name + "is purchasing groceries";
+    },
+    groceryCart: ["bread", "alfredo sauce", "chicken breasts", "fettucini pasta",]
+}
