76976c3a0119f12cfac2fbf4d0e853f11ecc94a5
shaldavis <shal.davis@bryanuniversity.edu>
2020-09-14 18:26:13 -0400

    Week 3 PNP assignment 3

---
+++ week3/functions.js
@@ -0,0 +1,28 @@
+//problem 1
+function sum(numb1, numb2){
+    return numb1 + numb2
+}
+var result = sum(15 , 20)
+console.log(result)
+
+//problem 2
+function largeOf3(num1, num2, num3){
+    return Math.max.apply(Math, array)
+}
+let array = [12, 98, 57]
+console.log(largeOf3(array))
+
+//problem 3
+function oddOrEven(num) {
+    if(num % 2 == 0) {
+        return "Even";
+    }
+    else if(num % 2 == 1){
+        return "Odd";
+    }
+}
+console.log(oddOrEven(4));
+console.log(oddOrEven(5));
+
+//problem 4
+I do not understand this problem.
